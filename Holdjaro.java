8c49bc758b91448918f6c4297caf3542d576ae3f
SIgabor <79259040+SIgabor@users.noreply.github.com>
2023-11-16 18:47:13 +0100

    test

--- Holdjaro.java
+++ Holdjaro.java
@@ -11,21 +11,38 @@ public class Holdjaro {
             { 0, 1, 0, 0, 0, 0, 0, 0 }
     };
 
-    private static int currentPosition[] = { 0, 0};
-    private static char currentHeadning  = 'S';
-    private static int previousPosition[];  // átmeneti pozíció tárolás, ha akadály van az új pozíción akkor visszaállítás
-    private static char previousHeading;  // átmeneti heading tárolás, ha akadály van az új pozíción akkor visszaállítás
+    private static int currentPosition[] = {5, 1};
+    private static char currentHeadning  = 'E';
+    private static int previousPosition[] = {0, 0};  // átmeneti pozíció tárolás, ha akadály van az új pozíción akkor visszaállítás
+    private static char previousHeading = 'X';  // átmeneti heading tárolás, ha akadály van az új pozíción akkor visszaállítás
 
     public static void main(String args[]) {
 
-        Move('b');
+        DrawMatrix();
+
+        Move('f');
         System.out.println(" " + currentHeadning + currentPosition[0] + currentPosition[1]);
+        DrawMatrix();
 
         
         Turn('r');
 
         
 
+    }
+
+    private static void DrawMatrix(){
+        int helper;
+        helper = map[currentPosition[0]][currentPosition[1]];
+        map[currentPosition[0]][currentPosition[1]] = 8;
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n\n");
+        map[currentPosition[0]][currentPosition[1]] = helper;
     }
 
     private static void Move(char direction){
@@ -35,6 +52,10 @@ public class Holdjaro {
             switch (currentHeadning) {
                 case 'N':
 
+                    previousHeading = currentHeadning;
+                    for(int i = 0; i < currentPosition.length; i++){
+                        previousPosition[i] = currentPosition[i];
+                    }
                     if(currentPosition[0] == 0){
                         if(currentPosition[1] >= 0 && currentPosition[1] <= 3){
                             currentPosition[1] += 4;
@@ -44,6 +65,12 @@ public class Holdjaro {
                         currentHeadning = 'S';
                     }else{
                         currentPosition[0] -= 1;
+                    }
+                    if(map[currentPosition[0]][currentPosition[1]] == 1){
+                        currentHeadning = previousHeading;
+                        for(int i = 0; i < currentPosition.length; i++){
+                        currentPosition[i] = previousPosition[i];
+                        }
                     }
                     
                     break;
