587a5e21de84f43be51185665a3b1d9d456bfc8f
SIgabor <79259040+SIgabor@users.noreply.github.com>
2023-11-19 21:30:02 +0100

    Move + MoveTest + CollisionTest

--- Holdjaro.java
+++ Holdjaro.java
@@ -19,9 +19,199 @@ public class Holdjaro {
 
     public static void main(String args[]) {
 
+        Move('b');
+        System.out.println(" " + currentHeadning + currentPosition[0] + currentPosition[1]);
+
+        Turn('r');
+
     }
 
-    
+    public static String Move(char direction) {
+
+        Task: switch (direction) {
+
+            case 'f':
+                switch (currentHeadning) {
+                    case 'N':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[0] == 0) {
+                            if (currentPosition[1] >= 0 && currentPosition[1] <= 3) {
+                                currentPosition[1] += 4;
+                            } else if (currentPosition[1] >= 4 && currentPosition[1] <= 7) {
+                                currentPosition[1] -= 4;
+                            }
+                            currentHeadning = 'S';
+                        } else {
+                            currentPosition[0] -= 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                    case 'S':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[0] == 7) {
+                            if (currentPosition[1] >= 0 && currentPosition[1] <= 3) {
+                                currentPosition[1] += 4;
+                            } else if (currentPosition[1] >= 4 && currentPosition[1] <= 7) {
+                                currentPosition[1] -= 4;
+                            }
+                            currentHeadning = 'N';
+                        } else {
+                            currentPosition[0] += 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                    case 'E':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[1] == 7) {
+                            currentPosition[1] = 0;
+                        } else {
+                            currentPosition[1] += 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                    case 'W':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[1] == 0) {
+                            currentPosition[1] = 7;
+                        } else {
+                            currentPosition[1] -= 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                }
+
+            case 'b':
+                switch (currentHeadning) {
+                    case 'N':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[0] == 7) {
+                            if (currentPosition[1] >= 0 && currentPosition[1] <= 3) {
+                                currentPosition[1] += 4;
+                            } else if (currentPosition[1] >= 4 && currentPosition[1] <= 7) {
+                                currentPosition[1] -= 4;
+                            }
+                            currentHeadning = 'S';
+                        } else {
+                            currentPosition[0] += 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                    case 'S':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[0] == 0) {
+                            if (currentPosition[1] >= 0 && currentPosition[1] <= 3) {
+                                currentPosition[1] += 4;
+                            } else if (currentPosition[1] >= 4 && currentPosition[1] <= 7) {
+                                currentPosition[1] -= 4;
+                            }
+                            currentHeadning = 'N';
+                        } else {
+                            currentPosition[0] -= 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                    case 'E':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[1] == 0) {
+                            currentPosition[1] = 7;
+                        } else {
+                            currentPosition[1] -= 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+
+                    case 'W':
+                        previousHeading = currentHeadning;
+                        for (int i = 0; i < currentPosition.length; i++) {
+                            previousPosition[i] = currentPosition[i];
+                        }
+                        if (currentPosition[1] == 7) {
+                            currentPosition[1] = 0;
+                        } else {
+                            currentPosition[1] += 1;
+                        }
+                        if (map[currentPosition[0]][currentPosition[1]] == 1) {
+                            currentHeadning = previousHeading;
+                            for (int i = 0; i < currentPosition.length; i++) {
+                                currentPosition[i] = previousPosition[i];
+                            }
+                        }
+
+                        break Task;
+                }
+        }
+        return currentHeadning + "" + currentPosition[0] + currentPosition[1];
+    }
+
     public static char Turn(char direction) {
 
         if (direction == 'r') {
--- HoldjaroTest.java
+++ HoldjaroTest.java
@@ -2,9 +2,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class HoldjaroTest {
+
     @Test
     public void TurnTest(){
         Assert.assertEquals('W', Holdjaro.Turn('l'));
     }
-    
+
+    @Test
+    public void MoveTest(){
+        Assert.assertEquals("N23", Holdjaro.Move('f'));
+    }
+
+    @Test
+    public void CollisionTest(){
+        Assert.assertEquals("N33", Holdjaro.Move('b'));
+    }
 }
+
